a2f3c968904ea28cc433d5c64b873f65c73bd6e5
Zahin Ukasyah <zhnuksyh03@gmail.com>
2025-12-29 20:09:26 +0800

    Setup PWA and switch to GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg'],
+            manifest: {
+                name: 'Ditto! Memory Game',
+                short_name: 'Ditto',
+                theme_color: '#ffffff',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
     base: "/ditto/",
 })
